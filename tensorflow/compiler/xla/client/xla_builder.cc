1f9f9967b351593d74725cdf45040b3571fdc775
Lianmin Zheng <lianminzheng@gmail.com>
2022-11-20 09:14:42 +0000

    Add changes under tensorflow/compiler/xla/client/*

--- tensorflow/compiler/xla/client/xla_builder.cc
+++ tensorflow/compiler/xla/client/xla_builder.cc
@@ -3386,7 +3386,8 @@ XlaOp XlaBuilder::AllToAll(XlaOp operand, int64_t split_dimension,
                            int64_t concat_dimension, int64_t split_count,
                            absl::Span<const ReplicaGroup> replica_groups,
                            const std::optional<Layout>& layout,
-                           const std::optional<ChannelHandle>& channel_id) {
+                           const std::optional<ChannelHandle>& channel_id,
+                           const std::optional<bool> use_global_device_ids) {
   // Array all_to_all may need to violate layout constraint to be legal so use
   // the tuple version.
   if (layout.has_value()) {
@@ -3394,13 +3395,14 @@ XlaOp XlaBuilder::AllToAll(XlaOp operand, int64_t split_dimension,
                          split_count, replica_groups, layout, channel_id);
   }
   return AllToAllArray(operand, split_dimension, concat_dimension, split_count,
-                       replica_groups, channel_id);
+                       replica_groups, channel_id, use_global_device_ids);
 }
 
 XlaOp XlaBuilder::AllToAllArray(
     XlaOp operand, int64_t split_dimension, int64_t concat_dimension,
     int64_t split_count, absl::Span<const ReplicaGroup> replica_groups,
-    const std::optional<ChannelHandle>& channel_id) {
+    const std::optional<ChannelHandle>& channel_id,
+    const std::optional<bool> use_global_device_ids) {
   return ReportErrorOrReturn([&]() -> StatusOr<XlaOp> {
     TF_ASSIGN_OR_RETURN(const Shape* operand_shape, GetShapePtr(operand));
     TF_ASSIGN_OR_RETURN(
@@ -3419,9 +3421,14 @@ XlaOp XlaBuilder::AllToAllArray(
         *instr.add_replica_groups() = group;
       }
     }
+
     instr.add_dimensions(split_dimension);
     if (channel_id.has_value()) {
       instr.set_channel_id(channel_id->handle());
+    }
+    // TODO(Maozhou): to confirm
+    if (use_global_device_ids.has_value()) {
+      instr.set_use_global_device_ids(use_global_device_ids.value());
     }
     TF_ASSIGN_OR_RETURN(
         XlaOp all_to_all,
@@ -5008,10 +5015,11 @@ XlaOp AllToAll(const XlaOp operand, int64_t split_dimension,
                int64_t concat_dimension, int64_t split_count,
                absl::Span<const ReplicaGroup> replica_groups,
                const std::optional<Layout>& layout,
-               const std::optional<ChannelHandle>& channel_id) {
+               const std::optional<ChannelHandle>& channel_id,
+               const std::optional<bool> use_global_device_ids) {
   return operand.builder()->AllToAll(operand, split_dimension, concat_dimension,
                                      split_count, replica_groups, layout,
-                                     channel_id);
+                                     channel_id, use_global_device_ids);
 }
 
 XlaOp AllToAllTuple(absl::Span<const XlaOp> operands,
